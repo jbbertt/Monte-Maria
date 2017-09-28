3195a584eaa2fd38400ce75204f73a5cdfe37f0f
jbbertt <31472515+jbbertt@users.noreply.github.com>
2017-09-28 19:37:21 +0800

    log in form not finished

---
+++ MMPS/MMPS/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jazzybert26
+ * Date: 9/28/2017
+ * Time: 7:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MMPS
+{
+	partial class log_in_form
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.panel7 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel8 = new System.Windows.Forms.Panel();
+			this.panel9 = new System.Windows.Forms.Panel();
+			this.panel10 = new System.Windows.Forms.Panel();
+			this.panel11 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel6.SuspendLayout();
+			this.panel7.SuspendLayout();
+			this.panel8.SuspendLayout();
+			this.panel10.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel4);
+			this.panel1.Controls.Add(this.panel3);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(657, 401);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.panel6);
+			this.panel4.Controls.Add(this.panel5);
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel4.Location = new System.Drawing.Point(0, 82);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(657, 276);
+			this.panel4.TabIndex = 2;
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.SystemColors.Control;
+			this.panel6.Controls.Add(this.panel8);
+			this.panel6.Controls.Add(this.panel7);
+			this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel6.Location = new System.Drawing.Point(208, 0);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(449, 276);
+			this.panel6.TabIndex = 1;
+			// 
+			// panel7
+			// 
+			this.panel7.Controls.Add(this.label1);
+			this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel7.Location = new System.Drawing.Point(0, 0);
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new System.Drawing.Size(449, 48);
+			this.panel7.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(449, 48);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Please Enter Your Log In details :";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel5.Location = new System.Drawing.Point(0, 0);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(208, 276);
+			this.panel5.TabIndex = 0;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel3.Location = new System.Drawing.Point(0, 358);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(657, 43);
+			this.panel3.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(657, 82);
+			this.panel2.TabIndex = 0;
+			// 
+			// panel8
+			// 
+			this.panel8.Controls.Add(this.panel10);
+			this.panel8.Controls.Add(this.panel9);
+			this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel8.Location = new System.Drawing.Point(0, 48);
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new System.Drawing.Size(449, 228);
+			this.panel8.TabIndex = 1;
+			// 
+			// panel9
+			// 
+			this.panel9.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel9.Location = new System.Drawing.Point(0, 172);
+			this.panel9.Name = "panel9";
+			this.panel9.Size = new System.Drawing.Size(449, 56);
+			this.panel9.TabIndex = 0;
+			// 
+			// panel10
+			// 
+			this.panel10.BackColor = System.Drawing.SystemColors.Control;
+			this.panel10.Controls.Add(this.panel11);
+			this.panel10.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel10.Location = new System.Drawing.Point(0, 0);
+			this.panel10.Name = "panel10";
+			this.panel10.Size = new System.Drawing.Size(449, 172);
+			this.panel10.TabIndex = 1;
+			// 
+			// panel11
+			// 
+			this.panel11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel11.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel11.Location = new System.Drawing.Point(0, 0);
+			this.panel11.Name = "panel11";
+			this.panel11.Size = new System.Drawing.Size(144, 172);
+			this.panel11.TabIndex = 0;
+			// 
+			// log_in_form
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(657, 401);
+			this.Controls.Add(this.panel1);
+			this.Name = "log_in_form";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Log In ";
+			this.panel1.ResumeLayout(false);
+			this.panel4.ResumeLayout(false);
+			this.panel6.ResumeLayout(false);
+			this.panel7.ResumeLayout(false);
+			this.panel8.ResumeLayout(false);
+			this.panel10.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Panel panel11;
+		private System.Windows.Forms.Panel panel9;
+		private System.Windows.Forms.Panel panel10;
+		private System.Windows.Forms.Panel panel8;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel7;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ MMPS/MMPS/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jazzybert26
+ * Date: 9/28/2017
+ * Time: 7:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MMPS
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class log_in_form : Form
+	{
+		public log_in_form()
+		{
+			// 
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
