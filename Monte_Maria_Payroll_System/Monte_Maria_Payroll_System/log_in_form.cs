60b9c6ff19846ce9b71ffd6a00b31817aae70df1
jbbertt <31472515+jbbertt@users.noreply.github.com>
2017-10-07 20:35:15 +0800

    MMPS class and MMPS project

---
+++ Monte_Maria_Payroll_System/Monte_Maria_Payroll_System/log_in_form.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jazzybert26
+ * Date: 10/7/2017
+ * Time: 3:32 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Monte_Marial_Lib;
+
+namespace Monte_Maria_Payroll_System
+{
+	/// <summary>
+	/// Description of log_in_form.
+	/// </summary>
+	public partial class log_in_form : Form
+	{
+		Objlog_in log_in = new Objlog_in();
+		
+		public log_in_form()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		//wala ni function
+		void Label1Click(object sender, EventArgs e)
+		{		
+		}
+		
+		//exit btn ni
+		void Button2Click(object sender, EventArgs e)
+		{
+			DialogResult answer = MessageBox.Show("Are your sure you want to Exit the program?", "Confirm ", MessageBoxButtons.YesNo);
+			//this.Close();
+			if(answer == DialogResult.Yes){
+				this.Close();			}
+		}
+		
+		void Login_btnClick(object sender, EventArgs e)
+		{
+			if (tb_username.Text.Length > 0 && tb_password.Text.Length > 0)
+            {
+				
+				
+				this.Hide();
+				new MainForm().ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Log-in Details. Please enter a correct password or user-name", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } 
+			
+		}
+	}
+}
---
+++ Monte_Marial_Lib/Monte_Marial_Lib/MyClass.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jazzybert26
+ * Date: 10/7/2017
+ * Time: 4:31 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using bggPgSql;
+using Npgsql;
+
+namespace Monte_Marial_Lib
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class log_in
+	{
+		string username;
+		
+		public string Username {
+			get { return username; }
+			set { username = value; }
+		}
+		string password;
+		
+		public string Password {
+			get { return password; }
+			set { password = value; }
+		}
+		string role;
+		
+		public string Role {
+			get { return role; }
+			set { role = value; }
+		}
+		
+	}
+	    public class Objlog_in : log_in
+	    {
+	    bggQueryPgSql query = new bggQueryPgSql();
+        string myString;
+        
+        public bool Log_in(string username, password)
+        {
+        	myString = "Select role from log_in where username = '"+username+"' and password = '"+password+"'";
+        	return query.ViaSingleData(myString);
+        	
+        }
+        
+        
+        
+	    }
+}
